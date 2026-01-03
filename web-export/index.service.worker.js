c5b17a6a2879d4bf3bd77bb78ee6d9599eb591ea
Keiran <keiranbrown95@gmail.com>
2026-01-03 14:15:08 +0000

    Updated export

--- web-export/index.service.worker.js
+++ web-export/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1767449287|127700157';
+const CACHE_VERSION = '1767449621|461917921';
 /** @type {string} */
 const CACHE_PREFIX = 'Pong-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
